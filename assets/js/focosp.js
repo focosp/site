755cd0d6c815715390797b317b6b7922924b0ac2
Clederson Cruz <ccruz@focosp.com>
2019-03-19 15:32:01 -0300

    Some settings and adjusts for production

--- assets/js/focosp.js
+++ assets/js/focosp.js
@@ -1,15 +1,34 @@
 /* accordion da parte de soluções */
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function(){
-		this.classList.toggle("active");
-		var panel = this.nextElementSibling;
-		if (panel.style.display == "block") {
-			panel.style.display = "none";
-		} else {
-			panel.style.display = "block";
-		}
-	});
+function initComponents() {
+    initAccordion();
+
+    document.getElementById("everest-btn-external-chat")
+        .addEventListener("onclick", removeChatButton);
 }
+
+
+function initAccordion() {
+    var accordions = document.querySelectorAll(".accordion");
+
+    accordions.forEach( acc => {
+        acc.onclick = function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+
+            if (panel.style.display == "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        };
+    });
+}
+
+/*
+function removeChatButton() {
+    let button = document.getElementById("everest-btn-external-chat");
+    //button.parentNode.removeChild(button);
+}*/
+
+initComponents();
